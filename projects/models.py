3943a8097226c0a4bb77cc45f3291fc2e14ff9b6
Ismail Dhorat <ismail@zyelabs.net>
2010-10-09 17:31:41 +0200

    updating models

--- projects/models.py
+++ projects/models.py
@@ -1,16 +1,24 @@
 # Project Models
 from django.db import models
 from zyelabs.models import Base, Image
-from technologies.models import Technology
 
 class Project(Base):
     start_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)
-    technologies = models.ManyToManyField(Technology)
+    technologies = models.ManyToManyField('Technology', blank=True, null=True)
     image = models.ForeignKey(Image)
 
     class Meta:
         ordering = ['-start_date', '-end_date', ]
+
+    def __unicode__(self):
+        return self.title
+
+class Technology(Base):
+    image = models.ForeignKey(Image)
+
+    class Meta:
+        ordering = ['title']
 
     def __unicode__(self):
         return self.title
