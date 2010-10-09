2ecdf71d34add2b65f4e5112ed9dab02d82a93ac
Ismail Dhorat <ismail@zyelabs.net>
2010-10-09 02:36:42 +0200

    adding admin, updating models

---
+++ projects/admin.py
@@ -0,0 +1,7 @@
+# Projects admin
+from django.contrib import admin
+from models import Project
+
+admin.site.register(Project)
+
+
--- projects/models.py
+++ projects/models.py
@@ -1,11 +1,13 @@
+# Project Models
 from django.db import models
-from zyelabs.models import *
+from zyelabs.models import Base, Image
+from technologies.models import Technology
 
-class Project(base):
+class Project(Base):
     start_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)
-    technologies = models.ManyToManyField('Technology')
-    image = models.ForeignKey('Image')
+    technologies = models.ManyToManyField(Technology)
+    image = models.ForeignKey(Image)
 
     class Meta:
         ordering = ['-start_date', '-end_date', ]
