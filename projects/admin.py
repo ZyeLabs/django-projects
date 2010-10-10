0e7cb44f8fba7336db176bd5f61ebceb6ed23a2a
Ismail Dhorat <ismail@zyelabs.net>
2010-10-10 17:46:15 +0200

    updating admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,10 +1,8 @@
 # Projects admin
 from django.contrib import admin
+from zyelabs.admin import BaseAdmin
 from projects.models import Project, Technology
 
-class BaseAdmin(admin.ModelAdmin):
-    list_display = ('title','url','short_description')
-    prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(Project, BaseAdmin)
 admin.site.register(Technology, BaseAdmin)
